d44f8fc092e87a6ff48bf61940f441d2ed399456
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-10 21:38:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
 
+def sort_array_asc(array)
+  
+  array.sort
+  
+end
+
+
+def sort_array_desc(array)
+  
+  
+end
+
+
+def sort_array_char_count(array)
+  
+end
+
+
+def swap_elements(array)
+  
+end

3d5ca3915d48cdb3bcb1d4c712af5065aaa508a5
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-10 22:11:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
 
+def sort_array_asc(array)
+  
+  array.sort
+  
+end
+
+
+def sort_array_desc(array)
+  
+  array.sort {|a, b| b <=> a }
+  
+end
+
+
+def sort_array_char_count(array)
+  
+  array.sort {|a,b| a.size <=> b.size }
+  
+end
+
+
+def swap_elements(array)
+  
+  array[1], array[2] = array[2], array[1]
+  
+  array
+end
+
+def reverse_array(array)
+  
+  array.reverse
+  
+end
+
+def kesha_maker(array)
+  
+  array.map {|element| element[2] = "$" }
+  
+  array
+end
+
+def find_a(array)
+  
+  array.select {|element| element[0].downcase == "a"}
+  
+end
+
+def sum_array(array)
+  
+  array.inject(:+)
+  
+end
+
+def add_s(array)
+  
+  array.each_with_index {|elem, index| index != 1 ? elem << "s" : next }
+  
+end
